c156fec668758bd84dd5c81ad09ad34662ad6153
raghavakumar <raghava.crl@gmail.com>
2014-04-07 16:25:00 +0530

    0# Modified script to fetch cassandra source from url.
    Change-Id: I8cc3872aac0c249a8aa4c99040279788703e3f1b

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,181 +3,189 @@
 class cassandra {
 
   require cassandra::params
+
+  Exec
+        {
+           path => [ "/usr/bin", "/bin", "/usr/sbin" ]
+        }
+
   group { "${cassandra::params::cassandra_group}":
-		ensure => present,
-		gid => "900",
+           ensure => present,
+	   gid => "900",
 	}
 
-	user { "${cassandra::params::cassandra_owner}":
-		ensure => present,
-		comment => "Cassandra",
-		password => "!!",
-		uid => "900",
-		gid => "900",
-		shell => "/bin/bash",
-		home => "/home/${cassandra::params::cassandra_owner}",
-		require => Group["${cassandra::params::cassandra_group}"],
-	}
+  user { "${cassandra::params::cassandra_owner}":
+	  ensure => present,
+	  comment => "Cassandra",
+	  password => "!!",
+	  uid => "900",
+	  gid => "900",
+	  shell => "/bin/bash",
+	  home => "/home/${cassandra::params::cassandra_owner}",
+	  require => Group["${cassandra::params::cassandra_group}"],
+       }
 	
-	file { "/home/${cassandra::params::cassandra_owner}/.bash_profile":
-		ensure => present,
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-bash_profile",
-		content => template("cassandra/home/bash_profile.erb"),
-		require => User["${cassandra::params::cassandra_owner}"]
-	}
+  file { "/home/${cassandra::params::cassandra_owner}/.bash_profile":
+   	 ensure => present,
+	 owner => "${cassandra::params::cassandra_owner}",
+	 group => "${cassandra::params::cassandra_group}",
+	 alias => "cassandra-bash_profile",
+	 content => template("cassandra/home/bash_profile.erb"),
+	 require => User["${cassandra::params::cassandra_owner}"]
+       }
 		
-	file { "/home/${cassandra::params::cassandra_owner}":
-		ensure => "directory",
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-home",
-		require => [ User["${cassandra::params::cassandra_owner}"], Group["${cassandra::params::cassandra_group}"] ],
+  file { "/home/${cassandra::params::cassandra_owner}":
+  	 ensure => "directory",
+	 owner => "${cassandra::params::cassandra_owner}",
+	 group => "${cassandra::params::cassandra_group}",
+	 alias => "cassandra-home",
+	 require => [ User["${cassandra::params::cassandra_owner}"], Group["${cassandra::params::cassandra_group}"] ],
+       }
+
+  file { "${cassandra::params::cassandra_base}":
+	 ensure => "directory",
+	 owner  => "${cassandra::params::cassandra_owner}",
+	 group  => "${cassandra::params::cassandra_group}",
+	 alias  => "cassandra-base",
+         mode  => 755 
 	}
 
-  file {"${cassandra::params::cassandra_base}":
-		ensure => "directory",
-		owner  => "${cassandra::params::cassandra_owner}",
-		group  => "${cassandra::params::cassandra_group}",
-		alias  => "cassandra-base",
-    mode   => 755 
-	}
-
-  file {"${cassandra::params::cassandra_log_path}":
-		ensure => "directory",
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-log-path",
-    require => File["cassandra-base"]
-	}
+  file { "${cassandra::params::cassandra_log_path}":
+	 ensure => "directory",
+	 owner => "${cassandra::params::cassandra_owner}",
+	 group => "${cassandra::params::cassandra_group}",
+	 alias => "cassandra-log-path",
+         require => File["cassandra-base"]
+       }
 
 
-  file {"${cassandra::params::data_path}":
-    ensure => "directory",
-    purge  => true,
-    force  => true,
-    backup => false,
-		owner  => "${cassandra::params::cassandra_owner}",
-		group  => "${cassandra::params::cassandra_group}",
-		alias  => "cassandra-data-path",
-	}
+  file { "${cassandra::params::data_path}":
+	 ensure => "directory",
+         purge  => true,
+         force  => true,
+         backup => false,
+	 owner  => "${cassandra::params::cassandra_owner}",
+	 group  => "${cassandra::params::cassandra_group}",
+	 alias  => "cassandra-data-path",
+      }
 
- file {"${cassandra::params::commitlog_directory}":
-		ensure => "directory",
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-commitlog-directory",
-    require => File["cassandra-base"]
-	}
+ file { "${cassandra::params::commitlog_directory}":
+	ensure => "directory",
+	owner => "${cassandra::params::cassandra_owner}",
+	group => "${cassandra::params::cassandra_group}",
+	alias => "cassandra-commitlog-directory",
+        require => File["cassandra-base"]
+      }
 
- file {"${cassandra::params::saved_caches}":
-		ensure => "directory",
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-saved-caches",
-    require => File["cassandra-base"]
-	}
+ file { "${cassandra::params::saved_caches}":
+	ensure => "directory",
+	owner => "${cassandra::params::cassandra_owner}",
+	group => "${cassandra::params::cassandra_group}",
+	alias => "cassandra-saved-caches",
+        require => File["cassandra-base"]
+      }
+
+ exec { "download apache-cassandra-${cassandra::params::version}-bin.tar.gz":
+        command => "wget ${cassandra::params::download_url}",
+        cwd     => "${cassandra::params::cassandra_base}",
+        creates =>"${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}",
+        require => File["cassandra-base"],
+        user => "${cassandra::params::cassandra_owner}",                                                                                                          
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}-bin.tar.gz":
-    mode     => 0644,
-		owner    => "${cassandra::params::cassandra_owner}",
-		group    => "${cassandra::params::cassandra_group}",
-		source   => "puppet:///modules/cassandra/apache-cassandra-${cassandra::params::version}-bin.tar.gz",
-		alias    => "cassandra-source-tgz",
-    before   => Exec["untar-cassandra"],
-		require  => File["cassandra-base"]
-	}
- 
+        mode => 0644,
+        owner    => "${cassandra::params::cassandra_owner}",
+        group    => "${cassandra::params::cassandra_group}",
+        alias    => "cassandra-source-tgz",
+        before   => Exec["untar-cassandra"],
+        require  => File["cassandra-base"]
+      }
 
  exec { "untar apache-cassandra-${cassandra::params::version}-bin.tar.gz":
-    cwd          => "${cassandra::params::cassandra_base}", 
-    command      => "/bin/tar zxvf apache-cassandra-${cassandra::params::version}-bin.tar.gz",
-    creates      => "${cassandra::params::cassandra_base}/cassandra-${cassandra::params::version}",
-    alias        => "untar-cassandra",
-    refreshonly => true,
-    subscribe   => File["cassandra-source-tgz"],
-    user         => "cassandra",
-    before       => [ File[ "cassandra-symlink" ], File[ "cassandra-app-dir" ] ]
-	}
+         cwd => "${cassandra::params::cassandra_base}", 
+         command => "tar zxvf apache-cassandra-${cassandra::params::version}-bin.tar.gz",
+         creates => "${cassandra::params::cassandra_base}/cassandra-${cassandra::params::version}",
+         alias   => "untar-cassandra",
+         refreshonly => true,
+         subscribe   => File["cassandra-base"],
+         user => "${cassandra::params::cassandra_owner}",
+         before => [ File[ "cassandra-symlink" ], File[ "cassandra-app-dir" ] ]
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}":
-		ensure => "directory",
-		mode => 0644,
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		alias => "cassandra-app-dir",
-    before => [ File["cassandra-yaml"], File["cassandra-log4j"], File["cassandra-env"] ]
-	}
+	ensure => "directory",
+	mode => 0644,
+	owner => "${cassandra::params::cassandra_owner}",
+	group => "${cassandra::params::cassandra_group}",
+	alias => "cassandra-app-dir",
+        before => [ File["cassandra-yaml"], File["cassandra-log4j"], File["cassandra-env"] ]
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra":
-		force => true,
-		ensure => "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}",
-		alias => "cassandra-symlink",
-		owner => "${cassandra::params::cassandra_owner}",
-		group => "${cassandra::params::cassandra_group}",
-		require => File["cassandra-source-tgz"],
-	}
+	force => true,
+	ensure => "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}",
+	alias => "cassandra-symlink",
+	owner => "${cassandra::params::cassandra_owner}",
+	group => "${cassandra::params::cassandra_group}",
+	require => File["cassandra-base"],
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/cassandra.yaml":
-    alias => "cassandra-yaml",
-    content => template("cassandra/conf/cassandra.yaml.erb"),
-    owner => "${cassandra::params::cassandra_owner}",
-    group => "${cassandra::params::cassandra_group}",
-    mode => "644",
-    require => File["cassandra-app-dir"]
- }
+        alias => "cassandra-yaml",
+        content => template("cassandra/conf/cassandra.yaml.erb"),
+        owner => "${cassandra::params::cassandra_owner}",
+        group => "${cassandra::params::cassandra_group}",
+        mode => "644",
+        require => File["cassandra-app-dir"]
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/cassandra-env.sh":
-   alias => "cassandra-env",
-    content => template("cassandra/conf/cassandra-env.sh.erb"),
-    owner => "${cassandra::params::cassandra_owner}",
-    group => "${cassandra::params::cassandra_group}",
-    mode => "644",
-    require => File["cassandra-app-dir"]
- }
+        alias => "cassandra-env",
+        content => template("cassandra/conf/cassandra-env.sh.erb"),
+        owner => "${cassandra::params::cassandra_owner}",
+        group => "${cassandra::params::cassandra_group}",
+        mode => "644",
+        require => File["cassandra-app-dir"]
+      }
 
  file { "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/conf/log4j-server.properties":
-    alias => "cassandra-log4j",
-    content => template("cassandra/conf/log4j-server.properties.erb"),
-    owner => "${cassandra::params::cassandra_owner}",
-    group => "${cassandra::params::cassandra_group}",
-    mode => "644",
-    require => File["cassandra-app-dir"]
- }
+        alias => "cassandra-log4j",
+        content => template("cassandra/conf/log4j-server.properties.erb"),
+        owner => "${cassandra::params::cassandra_owner}",
+        group => "${cassandra::params::cassandra_group}",
+        mode => "644",
+        require => File["cassandra-app-dir"]
+      }
  
  file { "/etc/init.d/cassandra":
-    alias => "cassandra-initscript",
-    content => template("cassandra/conf/cassandra.sh.erb"),
-    owner => "${cassandra::params::cassandra_owner}",
-    group => "${cassandra::params::cassandra_group}",
-    mode => "755",
-    require => File["cassandra-app-dir"]
- }
+        alias => "cassandra-initscript",
+        content => template("cassandra/conf/cassandra.sh.erb"),
+        owner => "${cassandra::params::cassandra_owner}",
+        group => "${cassandra::params::cassandra_group}",
+        mode => "755",
+        require => File["cassandra-app-dir"]
+      }
  
  service { "${cassandra::params::service_name}":
-    ensure      => running,
-    enable      => true,
-    require     => File["cassandra-initscript"],
-    #hasstatus  => true,
-    #hasrestart => true,
-    alias       =>  "cassandra service",
- }
+           ensure => running,
+           enable => true,
+           require => File["cassandra-initscript"],
+           alias   =>  "cassandra service",
+         }
 
  exec { "wait_for_nodetool_status":
-     alias   => "wait_for_nodetool_status",
-     require => Service["${cassandra::params::service_name}"],
-     command => "sleep 15",
-     path    => "/usr/bin:/bin",
- }
+        alias   => "wait_for_nodetool_status",
+        require => Service["${cassandra::params::service_name}"],
+        command => "sleep 15",
+      }
 
- exec {"execute nodetool status":
-     alias     => "execute_nodetool_status",
-     require   => Service["${cassandra::params::service_name}"],
-     command   => "sh nodetool status",
-     path      => '/usr/bin:/bin',
-     cwd       => "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/bin",
-     user     => "${cassandra::params::cassandra_owner}",
-     logoutput => true,
- }
+ exec { "execute nodetool status":
+        alias  => "execute_nodetool_status",
+        require => Service["${cassandra::params::service_name}"],
+        command => "sh nodetool status",
+        cwd  => "${cassandra::params::cassandra_base}/apache-cassandra-${cassandra::params::version}/bin",
+        user => "${cassandra::params::cassandra_owner}",
+        logoutput => true,
+      }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,26 +4,31 @@
 class cassandra::params {
   
   $java_home = $::hostname ? {
-	default	=> "$(readlink -f /usr/bin/java | sed 's:bin/java::')",
-	}
-	$version = $::hostname ? {		
-		default	=> "2.0.4",
-	}
+     default => "$(readlink -f /usr/bin/java | sed 's:bin/java::')",
+  }
 
-	$cassandra_base = $::hostname ? {
-	  default	=> "/opt/cassandra",
-	}
+  $version = $::hostname ? {		
+     default => "2.0.4",
+  }
 
-	$data_path = $::hostname ? {
-		default	=> "/home/cassandra/data",
-	}
+  $download_url = $::hostname ? {
+     default => "https://archive.apache.org/dist/cassandra/2.0.4/apache-cassandra-2.0.4-bin.tar.gz",
+  }
+
+  $cassandra_base = $::hostname ? {
+     default => "/opt/cassandra",
+  }
+
+  $data_path = $::hostname ? {
+     default => "/home/cassandra/data",
+  }
 
   $commitlog_directory = $::hostname ? {
-    default => "/home/cassandra/commitlog",
+     default => "/home/cassandra/commitlog",
   }
 
   $saved_caches = $::hostname ? {
-    default => "/home/cassandra/saved_caches",
+     default => "/home/cassandra/saved_caches",
   }
 
   $cluster_name = $::hostname ? {
@@ -35,35 +40,34 @@ class cassandra::params {
   }
 
   $initial_token = $::hostname ? {
-    "agent1" => "42535295865117307932921825928971026432",
-    "agent2" => "85070591730234615865843651857942052864",
+     default => "42535295865117307932921825928971026432",
   } 
 
   $seeds = $::hostname ? {
-       default => "agent1",
+     default => "mastercentos.example.com",
   }
 
   $cassandra_log_path = $::hostname ? {
-      default => "/var/log/cassandra",
+     default => "/var/log/cassandra",
   }
 
   $jmx_port = $::hostname ? {
-      default => "7199",
+     default => "7199",
   }
 
   $max_heap = $::hostname ? {
-      default => "4G",
+     default => "4G",
   }
 
   $heap_newsize = $::hostname ? {
-      default => "800M",
+     default => "800M",
   }
 
   $cassandra_owner = $::hostname ? {
-      default => "cassandra",
+     default => "cassandra",
   }
 
   $cassandra_group = $::hostname ? {
-      default => "cassandra",
+     default => "cassandra",
   }
 }
